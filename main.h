c0a2fdc0ea42d40fc6073da752022dda19ca6883
Stevovenom <stephenokoth2002@gmail.com>
2023-09-19 17:02:19 -0700

    The update of main.h

--- main.h
+++ main.h
@@ -8,5 +8,9 @@
 #include <string.h>
 
 int _printf(const char *format, ...);
+int conversion_specifiers(va_list args);
+int function_for_dec(va_list args);
+int function_for_string(va_list args);
+int function_for_char(va_list args);
 
 #endif /* MAIN_H */
